2164ec4cf75b8d0d17ecb56bb062c28ca837dde4
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-09-08 12:54:20 +0200

    'Update'

--- SurfaceFitter3D/scripts/SurfaceFitter3D.lua
+++ SurfaceFitter3D/scripts/SurfaceFitter3D.lua
@@ -1,32 +1,4 @@
---[[----------------------------------------------------------------------------
 
-  Application Name:
-  SurfaceFitter3D
-
-  Summary:
-  Fitting planes and rectangles to surfaces in heightmaps
-
-  Description:
-  This Sample shows how to use PixelRegions and 3D shapes to select points for fitting.
-  Rectangles are fitted to different sets of points and the angle between planes is
-  calculated. The use of a plane as reference for the threshold function is illustrated.
-
-  How to Run:
-  Starting this sample is possible either by running the app (F5) or
-  debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
-  function allows debugging step-by-step after 'Engine.OnStarted' event.
-  Results can be seen in the 3D viewer and 2D viewer on the DevicePage.
-  Select Reflectance in the View: box at the top of the GUI and zoom in on the
-  data for best experience.
-  Restarting the Sample may be necessary to show the heightmap after loading the webpage.
-  To run this Sample a device with SICK Algorithm API and AppEngine >= V2.5.0 is
-  required. For example SIM4000 with latest firmware. Alternatively the Emulator
-  on AppStudio 2.3 or higher can be used.
-
-  More Information:
-  Tutorial "Algorithms - Fitting and Measurement".
-
-------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 
 local DELAY = 1000 -- ms between visualization steps for demonstration purpose
