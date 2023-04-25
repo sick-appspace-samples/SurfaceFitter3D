e7a30ba311d5ba9c978529e2ae964bd23e727193
GitLab Runner <gitlab-runner@ip-172-31-13-207.eu-central-1.compute.internal>
2023-04-25 18:30:26 +0000

    Update

--- SurfaceFitter3D/scripts/SurfaceFitter3D.lua
+++ SurfaceFitter3D/scripts/SurfaceFitter3D.lua
@@ -12,17 +12,15 @@ local viewer2D = View.create("viewer2D1")
 local searchRegionDecoration = View.PixelRegionDecoration.create()
 searchRegionDecoration:setColor(0, 255, 255, 60)
 
-local searchDecoration = View.ShapeDecoration.create()
-searchDecoration:setFillColor(0, 255, 255, 80)
-searchDecoration:setLineColor(0, 255, 255)
-searchDecoration:setLineWidth(3)
+local searchDecoration = View.ShapeDecoration.create():setLineWidth(3)
+searchDecoration:setFillColor(0, 255, 255, 80):setLineColor(0, 255, 255)
 
 -- Green color scheme for fitted rectangles using ransac.
+local foundDecoration = View.ShapeDecoration.create():setLineWidth(3)
+foundDecoration:setFillColor(0, 255, 0, 120):setLineColor(0, 255, 0)
 
-local foundDecoration = View.ShapeDecoration.create()
-foundDecoration:setFillColor(0, 255, 0, 120)
-foundDecoration:setLineColor(0, 255, 0)
-foundDecoration:setLineWidth(3)
+local imgDecoration = View.ImageDecoration.create()
+imgDecoration:setRange(103, 145)
 
 -- Create surface fitter. Set fit mode to RANSAC to be robust
 -- against outliers. Set the margin to 0.25 mm, based on the
@@ -36,14 +34,14 @@ local sfls = Image.SurfaceFitter.create()
 sfls:setFitMode('LEASTSQUARES')
 
 --Setting specific view ID for iconics in viewer
-local imViewId = 'Heightmap1'
 local hmViewId = 'Image1'
 
 --End of Global Scope-----------------------------------------------------------
 
 --Start of Function and Event Scope---------------------------------------------
 
---@findObjectBasePlane(hm:Image) : Shape3D
+---@param hm Image
+---@return Shape3D
 local function findObjectBasePlane(hm)
   -- The main plane of the object is located around 111 mm above the
   -- zero level. The surronding flat surfaces are at around 106 mm.
@@ -51,7 +49,10 @@ local function findObjectBasePlane(hm)
   return plane
 end
 
--- @findRectangleInBox(fitter:Image.SurfaceFitter, hm:Image, box:Shape3D) : Shape3D
+---@param fitter Image.SurfaceFitter
+---@param hm Image
+---@param box Shape3D
+---@return Shape3D
 local function findRectangleInBox(fitter, hm, box)
   -- Project the shape to the xy-plane, and note the extent in z.
   -- Fit a rectangle to that part of the heightmap.
@@ -59,18 +60,20 @@ local function findRectangleInBox(fitter, hm, box)
   local rect = fitter:fitRectangle(hm, boxRegion, {boxMinZ, boxMaxZ}, true)
 
   -- Updating view
-  viewer2D:addPixelRegion(boxRegion, searchRegionDecoration, nil, imViewId)
+  viewer2D:addPixelRegion(boxRegion, searchRegionDecoration)
   viewer2D:present()
 
-  viewer3D:addShape(box, searchDecoration, nil, hmViewId)
-  viewer3D:addShape(rect, foundDecoration, nil, hmViewId)
+  viewer3D:addShape(box, searchDecoration)
+  viewer3D:addShape(rect, foundDecoration)
   viewer3D:present()
 
   Script.sleep(DELAY) -- for demonstration purpose only
   return rect
 end
 
--- @findUpperRectangle(hm:Image, searchBaseRect:Shape3D) : Shape3D
+---@param hm Image
+---@param searchBaseRect Shape3D
+---@return Shape3D
 local function findUpperRectangle(hm, searchBaseRect)
   -- Finding areas 3 to 10 mm above the plane of the searchBaseRect.
   local upperRegion = Image.thresholdPlane(hm, 3, 10, searchBaseRect:toPlane())
@@ -82,19 +85,22 @@ local function findUpperRectangle(hm, searchBaseRect)
   local upperRect = sf:fitRectangle(hm, upperRegion:getIntersection(rectRegion), nil, false)
 
   -- Updating View
-  viewer2D:addPixelRegion(upperRegion, searchRegionDecoration, nil, imViewId)
-  viewer2D:addPixelRegion(rectRegion, searchRegionDecoration, nil, imViewId)
+  viewer2D:addPixelRegion(upperRegion, searchRegionDecoration)
+  viewer2D:addPixelRegion(rectRegion, searchRegionDecoration)
   viewer2D:present()
 
-  viewer3D:addShape(searchBaseRect, searchDecoration, nil, hmViewId)
-  viewer3D:addShape(upperRect, foundDecoration, nil, hmViewId)
+  viewer3D:addShape(searchBaseRect, searchDecoration)
+  viewer3D:addShape(upperRect, foundDecoration)
   viewer3D:present()
 
   Script.sleep(DELAY) -- for demonstration purpose only
   return upperRect
 end
 
--- @printPlaneTilt(base:Shape3D, tilted:Shape3D, tiltedLS:Shape3D, upper:Shape3D)
+---@param base Shape3D
+---@param tilted Shape3D
+---@param tiltedLS Shape3D
+---@param upper Shape3D
 local function printPlaneTilt(base, tilted, tiltedLS, upper)
   local basePlaneTilt = base:getIntersectionAngle(Shape3D.createPlane(0, 0, 1, 0))
   local tiltedToBaseRad = tilted:getIntersectionAngle(base)
@@ -128,11 +134,11 @@ local function main()
 
   -- Updating views
   viewer2D:clear()
-  viewer2D:addImage(heightmap, nil, imViewId)
+  viewer2D:addImage(heightmap)
   viewer2D:present()
 
   viewer3D:clear()
-  viewer3D:addHeightmap( {heightmap, intensitymap}, {}, {'Reflectance'}, hmViewId )
+  viewer3D:addHeightmap( {heightmap, intensitymap}, {imgDecoration}, {'Reflectance'}, hmViewId )
   viewer3D:present()
   Script.sleep(DELAY) -- for demonstration purpose only
 
@@ -142,7 +148,7 @@ local function main()
 
   -- Fit planes and rectangles
   local basePlane = findObjectBasePlane(heightmap)
-  viewer3D:addShape(basePlane, foundDecoration, nil, hmViewId)
+  viewer3D:addShape(basePlane, foundDecoration)
   viewer3D:present()
   Script.sleep(DELAY) -- for demonstration purpose only
 
--- readme.md
+++ readme.md
@@ -1,10 +1,15 @@
 ## SurfaceFitter3D
+
 Fitting planes and rectangles to surfaces in heightmaps
+
 ### Description
+
 This Sample shows how to use PixelRegions and 3D shapes to select points for fitting.
 Rectangles are fitted to different sets of points and the angle between planes is
 calculated. The use of a plane as reference for the threshold function is illustrated.
+
 ### How to Run
+
 Starting this sample is possible either by running the App (F5) or
 debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
 function allows debugging step-by-step after 'Engine.OnStarted' event.
@@ -15,8 +20,11 @@ Restarting the Sample may be necessary to show the heightmap after loading the w
 To run this Sample a device with SICK Algorithm API and AppEngine >= V2.5.0 is
 required. For example SIM4000 with latest firmware. Alternatively the Emulator
 on AppStudio 2.3 or higher can be used.
+
 ### More Information
+
 Tutorial "Algorithms - Fitting and Measurement"
 
 ### Topics
-Algorithm, Image-3D, Fitting, Measurement, Sample, SICK-AppSpace
+
+algorithm, image-3d, fitting, measurement, sample, sick-appspace
